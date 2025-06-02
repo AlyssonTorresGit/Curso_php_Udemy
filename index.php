faa6e48114bb1248c0f1c8c4119d38ba44c9f3f6
Alysson Torres <alysson-b@hotmail.com>
2025-06-02 11:56:06 -0300

    ewrfew

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
 <?php
+require 'vendor/autoload.php';
 
-// require 'sistema/configuracao.php';
-// include 'helpers.php';
-include './sistema/Nucleo/Mensagem.php';
-include './sistema/Nucleo/controlador.php';
-
-use sistema\Nucleo\controlador;
-
-$controlador = new controlador('admin');
-echo '<hr>';
-var_dump($controlador);
+echo sistema\Nucleo\Helpers::saudacao();
