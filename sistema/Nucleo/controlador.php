e9cfab612f1976e9d46dcac1b9b8e3384956428e
Alysson Torres <alysson-b@hotmail.com>
2025-06-10 10:51:47 -0300

    sdfsad

--- sistema/Nucleo/controlador.php
+++ sistema/Nucleo/controlador.php
@@ -7,7 +7,7 @@ use sistema\Suporte\Template;
 class Controlador
 {
     protected Template $template;
-    public function __construct(string $diretorio = null)
+    public function __construct(string $diretorio)
     {
         $this->template = new Template($diretorio);
     }
